d6c6ee901538935dddf0002b2bae5b1ce3355ba9
Kelxety <llanzanarobinjohn@gmail.com>
2023-01-01 02:18:08 +0800

    added navbar and calculator components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
